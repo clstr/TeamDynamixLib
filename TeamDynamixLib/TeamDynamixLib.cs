61b8dbd84616e00f3de7aad3e8d599c546e1c300
Sergio Palomino <serg2k9@AlphaOmega>
2019-07-24 18:19:05 -0400

    Removed unused code

--- TeamDynamixLib/TeamDynamixLib.cs
+++ TeamDynamixLib/TeamDynamixLib.cs
@@ -1,12 +1,3 @@
-﻿using System;
-using System.Net;
-using System.Threading.Tasks;
-
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using RestSharp;
-using TeamDynamix.Api.Tickets;
-
-namespace TeamDynamixLib {
+﻿namespace TeamDynamixLib {
 
 }
