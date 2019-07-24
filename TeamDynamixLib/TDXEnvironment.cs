065dcd61af8c54f98180c4025543afa5090b2e48
Sergio Palomino <serg2k9@AlphaOmega>
2019-07-24 18:18:10 -0400

    Removed unused 'usings'

--- TeamDynamixLib/TDXEnvironment.cs
+++ TeamDynamixLib/TDXEnvironment.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TeamDynamixLib {
+﻿namespace TeamDynamixLib {
     public class TDXEnvironment {
         public string ClientUrl {
             get; set;
