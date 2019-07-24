f9bfe0516a94278369ed5ccd34e0609b1f0395a1
palomins <palomins@MJ06LP3R>
2019-07-24 14:34:01 -0400

    Add project files.

---
+++ TeamDynamixLib/TDXEnvironment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamDynamixLib {
+    public class TDXEnvironment {
+        public string ClientUrl {
+            get; set;
+        }
+        public bool IsSandboxEnvironment {
+            get; set;
+        }
+        public string ProxyURL {
+            get; set;
+        }
+        public int ProxPort {
+            get; set;
+        }
+    }
+}
