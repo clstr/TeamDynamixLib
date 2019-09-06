32b66565588b2c056fddf4fc7898e3adbecf09b5
palomins <palomins@MJ06LP3R>
2019-09-06 11:44:32 -0400

    Added Test Project

---
+++ TeamDynamixLibTests/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+using TeamDynamixLib;
+using TeamDynamix.Api.Auth;
+
+namespace TeamDynamixLibTests {
+
+    class Program {
+
+        static async Task Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("appsettings.Development.json", true, true)
+                .Build();
+
+            var tdxConfig = new TDXSettings();
+            config.GetSection("TeamDynamixSettings").Bind(tdxConfig);
+
+            var proxyConfig = new ProxySettings();
+            config.GetSection("ProxySettings").Bind(proxyConfig);
+
+            TDXEnvironment tDXEnvironment = new TDXEnvironment {
+                ClientUrl = tdxConfig.ClientURL,
+                IsSandboxEnvironment = tdxConfig.IsSandboxEnvironment
+            };
+
+            AdminTokenParameters adminTokenParameters = new AdminTokenParameters {
+                BEID = tdxConfig.BEID,
+                WebServicesKey = tdxConfig.WebServiceKey
+            };
+
+            AuthenticationLib authenticationLib = new AuthenticationLib();
+            var JWT = await authenticationLib.GetAuthHeaderAsync(adminTokenParameters, tDXEnvironment);
+
+            TicketLib ticketLib = new TicketLib();
+            var ticketDetailsExample = await ticketLib.GetTicketAsync(9757500, 431, JWT, tDXEnvironment);
+
+            Console.WriteLine(ticketDetailsExample.Title);
+            Console.WriteLine(ticketDetailsExample.Description);
+            Console.WriteLine(ticketDetailsExample.RequestorName);
+            Console.WriteLine(ticketDetailsExample.StatusID);
+            Console.WriteLine(ticketDetailsExample.ResponsibleFullName);
+
+            Console.ReadLine();
+        }
+    }
+
+    class TDXSettings {
+        public Guid BEID {
+            get; set;
+        }
+        public Guid WebServiceKey {
+            get; set;
+        }
+        public string ClientURL {
+            get; set;
+        }
+        public bool IsSandboxEnvironment {
+            get; set;
+        }
+    }
+
+    class ProxySettings {
+        public string ProxyIP {
+            get; set;
+        }
+        public short ProxyPort {
+            get; set;
+        }
+    }
+
+}
